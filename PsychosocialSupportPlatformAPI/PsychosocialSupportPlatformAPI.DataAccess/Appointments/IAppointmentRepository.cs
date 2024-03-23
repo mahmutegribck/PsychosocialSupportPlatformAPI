d1dbeac022dfa81edac4b9ea3171d1d4da69087d
mahmutegribck <megribck01@gmail.com>
2024-03-23 12:55:46 +0300

    Added IAppointmentRepository.

---
+++ PsychosocialSupportPlatformAPI/PsychosocialSupportPlatformAPI.DataAccess/Appointments/IAppointmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using PsychosocialSupportPlatformAPI.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsychosocialSupportPlatformAPI.DataAccess.Appointments
+{
+    public interface IAppointmentRepository
+    {
+        Task CreateAppointment(Appointment appointment);
+        Task DeleteAppointment(Appointment appointment);
+        Task UpdateAppointment(Appointment appointment);
+        Task<Appointment> GetAppointment(Appointment appointment);
+        Task<List<Appointment>> GetPatientAppointments();
+        Task<List<Appointment>> GetDoctorAppointments();
+    }
+}
